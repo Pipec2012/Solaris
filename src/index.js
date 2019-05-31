a0fcfe7bfe0604d7253a1680e9ca0bd3c979b86d
Pipec2012 <nampipec@gmail.com>
2019-05-31 15:42:47 +0500

    sending form by mail

--- src/index.js
+++ src/index.js
@@ -96,6 +96,26 @@ $(function () {
             }
         }
     })
+    $("#form").submit(function() {
+        var str = $(this).serialize();
+        
+        $.ajax({
+        type: "POST",
+        url: "https://pipec2012.github.io/Solaris/contact.php",
+        data: str,
+        success: function(msg) {
+            if(msg == 'OK') {
+            result = '<p>Ваш заказ принят</p>';
+            $(".fields").hide();
+        } else {
+            result = msg;
+        }
+        $('.note').html(result);
+        }
+            });
+                return false;
+            });
+        
     // if($(window).width() <= 700){
         
     // } 
