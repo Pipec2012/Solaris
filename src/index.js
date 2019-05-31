f67a3cd6d86cd16cc28e7ba173c465d25e5e04a8
Pipec2012 <nampipec@gmail.com>
2019-05-31 11:35:24 +0500

    change the display of contacts and product cards

--- src/index.js
+++ src/index.js
@@ -6,7 +6,41 @@ import "./jquery.fullPage.min.js";
 // import "./blocks/gallery/gallery";
 import "./owl.carousel.min.js";
 
+// const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
+//       animationTime = 300,
+//       framesCount = 20;
 
+// anchors.forEach(function(item) {
+//   // каждому якорю присваиваем обработчик события
+//   item.addEventListener('click', function(e) {
+//     // убираем стандартное поведение
+//     e.preventDefault();
+    
+//     // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+//     let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+    
+//     // запускаем интервал, в котором
+//     let scroller = setInterval(function() {
+//       // считаем на сколько скроллить за 1 такт
+//       let scrollBy = coordY / framesCount;
+      
+//       // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+//       // и дно страницы не достигнуто
+//       if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+//         // то скроллим на к-во пикселей, которое соответствует одному такту
+//         window.scrollBy(0, scrollBy);
+//       } else {
+//         // иначе добираемся до элемента и выходим из интервала
+//         window.scrollTo(0, coordY);
+//         clearInterval(scroller);
+//       }
+//     // время интервала равняется частному от времени анимации и к-ва кадров
+//     }, animationTime / framesCount);
+//   });
+// });
+
+
+  
 $(function () {
     // $.firefly({
     //     color: '#fff',
@@ -16,8 +50,7 @@ $(function () {
     //     on: '#firefly',
     //     borderRadius: 50
     // });
-    
-    
+    $("html,body").animate({scrollTop:0},500);
 
     $(document).ready(function() {
         if($(window).width() >= 700){
@@ -27,14 +60,19 @@ $(function () {
                 css3: true,
                 scrollHorizontally: false,
                 scrollingSpeed: 1000,
-                touchSensitivity: 10
+                touchSensitivity: 10,
+                loopTop: true,
+                loopBottom: true
             });
         } 
         // else {
-        //     $('#fullpage').fullpage({
-        //         scrollOverflow: false,
-        //         resize : false
-        //     });
+        //     var $page = $('html, body');
+        //     $('a[href*="#"]').click(function() {
+        //     $page.animate({
+        //         scrollTop: $($.attr(this, 'href')).offset().top
+        //     }, 800);
+        //     return false;
+        // });
         // }
     });
     
@@ -58,8 +96,9 @@ $(function () {
             }
         }
     })
-
-    
+    // if($(window).width() <= 700){
+        
+    // } 
     // $(document).ready(function(){
     //     if($(window).width() <= 950){
     //     // Находим блок карусели
